f5584d656abcb7b04a94575dd73f31c3001b1b44
Gareth <gdavis@softwareinstitute.com>
2021-10-27 16:04:08 +0100

    first commit

---
+++ src/test/java/com/institute/software/the/davis/gareth/catsSBDemo/CatsSbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.software.the.davis.gareth.catsSBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsSbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
